a0e4f22b531353d72cd4104bd5c26a577d31590d
Yudistiraa33 <inyomanyudistira33@gmail.com>
2025-08-06 11:15:43 +0800

    update preloader

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,12 @@ import App from './App.jsx'
 import { Navbar } from './components/Navbar.jsx'
 import 'remixicon/fonts/remixicon.css'
 import Footer from './components/Footer.jsx'
-import Prelouder from './components/PreLouder.jsx'
+import PreLoader from './components/PreLoader.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Prelouder /> 
+    <PreLoader />
     <div className="container mx-auto px-4">
     <Navbar />
     <App />
